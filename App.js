ab170db319c5022e80214a30cbd6c1df9fd27661
oroV2 <onkarpawar58@gmail.com>
2024-01-13 17:39:24 +0530

    Animated adding and making function for swipe left right

--- App.js
+++ App.js
@@ -1,14 +1,55 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Balls from './src/screens/Balls';
+import Deck from './src/screens/Deck';
+import React from 'react';
+import { Button, Card } from 'react-native-elements';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Balls />
-      <StatusBar style="auto" />
-    </View>
-  );
+const DATA = [
+  { id: 1, text: 'Card #1', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-04.jpg' },
+  { id: 2, text: 'Card #2', uri: 'http://www.fluxdigital.co/wp-content/uploads/2015/04/Unsplash.jpg' },
+  { id: 3, text: 'Card #3', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-09.jpg' },
+  { id: 4, text: 'Card #4', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-01.jpg' },
+  { id: 5, text: 'Card #5', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-04.jpg' },
+  { id: 6, text: 'Card #6', uri: 'http://www.fluxdigital.co/wp-content/uploads/2015/04/Unsplash.jpg' },
+  { id: 7, text: 'Card #7', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-09.jpg' },
+  { id: 8, text: 'Card #8', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-01.jpg' },
+];
+
+export default class App extends React.Component {
+
+  renderCard(item) {
+    return (
+      <Card
+        key={item.id}>
+        <Card.Image
+          style={{
+            resizeMode: 'cover',
+            zIndex: 1
+          }}
+          source={{ uri: item.uri }} />
+        <Card.Title>{item.text}</Card.Title>
+
+        <Button
+          icon={{ name: 'code' }}
+          backgroundColor='#03A9F4'
+          title='View Now!'
+        />
+      </Card>
+    );
+  }
+
+  render() {
+    return (
+      <View style={styles.container} >
+        <Deck
+          data={DATA}
+          renderCard={this.renderCard}
+          renderNoMoreCards={this.renderNoMoreCards}
+        />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
